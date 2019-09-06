e2c4fafacb80e8ac4ce19162c7c17bec998c3905
itbullet <eduard.shapirov@gmail.com>
2019-09-06 17:18:28 +0300

    checkiO house password

---
+++ checkiO/house_password.py
@@ -0,0 +1,22 @@
+import re
+
+def checkio(data: str) -> bool:
+
+    str1 = re.match(r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$", data)
+
+    #replace this for solution
+    return True if str1 else False
+
+#Some hints
+#Just check all conditions
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
